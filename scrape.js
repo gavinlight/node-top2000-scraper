8c2dfd07a5a3bdb55826c6884215efa19a28bb8c
Gavin Ligthart <gavin@Gavins-Macbook.local>
2018-10-29 09:43:13 +0100

    Add website scraper and API response for playlist

--- scrape.js
+++ scrape.js
@@ -1,4 +1,5 @@
 const rp = require('request-promise');
+const select = require('cheerio');
 
 const options = {
   uri: 'https://www.iheart.com/playlist/hip-hop-and-rb-top-50-312064750-ViFEMBbVN6YdFceSzi234o/',
@@ -7,14 +8,41 @@ const options = {
 
 const scrape = (req, res) => {
   rp(options)
-    .then((data) => {
-      console.log(data);
+    .then((html) => {
+      let playlist = [];
+      let songs = select('.playlistListContainer .playlistList > .playlist-row', html);
+
+      if (songs && songs.length) {
+        songs.each((i, song) => {
+          playlist.push({
+            id: i + 1,
+            song: select('[data-test="song-row-song-title"] a', song).text(),
+            album: select('.album-subtitle a', song).text(),
+            artist: select('.artist a', song).text(),
+          });
+        });
+
+        res.json({
+          type: 'success',
+          data: playlist,
+          message: 'Successfully scraped playlist'
+        });
+      } else {
+        res.json({
+          type: 'error',
+          data: 'No songs found',
+          message: 'An error occured'
+        })
+      }
     })
     .catch((err) => {
       console.log(err);
+      res.json({
+        type: 'error',
+        data: err,
+        message: 'An error occured'
+      })
     });
-
-  res.send('Scraping page...');
 };
 
 module.exports = scrape;
